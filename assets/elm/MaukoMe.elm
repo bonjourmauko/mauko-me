8cd21a642740dd95e01faeb928eefca7a465d08d
Mauko Quiroga <maukoquiroga@gmail.com>
2017-06-07 04:12:00 +0200

    Add index

--- assets/elm/MaukoMe.elm
+++ assets/elm/MaukoMe.elm
@@ -4,28 +4,27 @@ import Html exposing (..)
 
 -- MAIN
 
-main : Html String
 main =
-    view model
+    view
 
--- MODEL
-
-type alias Landing =
-    { catch: String
-    , blabla: String
-    }
-
-model : Landing
-model =
-    { catch  = "Tinder of yadda-yadda"
-    , blabla = "This is good shit..."
-    }
-
--- VIEW
-
-view : Landing -> Html String
-view model =
-    div []
-    [ h1 [] [ text model.catch ]
-    , h2 [] [ text model.blabla ]
+view : Html String
+view =
+    section []
+    [ h1 [] [ text "Bonjour." ]
+    , h2 [] [ text "I'm Mauko Quiroga and I build things." ]
+    , p  [] [ text "I build "
+            , strong [] [ text "software" ]
+            , text " in Ruby, Elixir and JavaScript." ]
+    , p  [] [ text "I build "
+            , strong [] [ text "teams" ]
+            , text " with love, agile and Kanban." ]
+    , p  [] [ text "I build "
+            , strong [] [ text "products" ]
+            , text " that solve problems." ]
+    , p  [] [ text "I build "
+            , strong [] [ text "organisations" ]
+            , text " with people." ]
+    , p  [] [ text "I build "
+            , strong [] [ text "digital public services" ]
+            , text "." ]
     ]
